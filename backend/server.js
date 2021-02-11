d2a4e057899c54c7938f337666db01c155ae367f
rohit gupta <rohitgupta.che18@itbhu.ac.in>
2021-02-11 17:15:19 +0530

    paypal added succesfully

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,12 @@ connectDB()
 
 const app = express()
 
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -23,6 +29,10 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+
+app.get('/api/config/paypal', (req, res) => {
+    res.send(process.env.PAYPAL_CLIENT_ID);
+})
 
 app.use(notFound)
 app.use(errorHandler)
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,34 +1,64 @@
-import React, { useEffect } from 'react'
+import React,{useEffect,useState} from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getOrderDetails } from '../actions/orderActions'
+import { getOrderDetails, payOrder } from '../actions/orderActions'
+import axios from 'axios'
+import { ORDER_PAY_RESET } from '../constants/orderConstants'
+import { PayPalButton } from 'react-paypal-button-v2' 
+
+
 
 const OrderScreen = ({ match }) => {
-  const orderId = match.params.id
+ 
+    const orderId = match.params.id
 
-  const dispatch = useDispatch()
+    const [sdkReady, setSdkReady] = useState(false)
 
-  const orderDetails = useSelector((state) => state.orderDetails)
-  const { order, loading, error } = orderDetails
+    const dispatch = useDispatch()
 
-  if (!loading) {
-    //   Calculate prices
-    const addDecimals = (num) => {
-      return (Math.round(num * 100) / 100).toFixed(2)
-    }
+    const orderDetails = useSelector(state => state.orderDetails)
+    const { order, loading, error } = orderDetails
 
-    order.itemsPrice = addDecimals(
-      order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-    )
-  }
+    const orderPay = useSelector(state => state.orderPay)
+    const { loading: loadingPay, success: successPay } = orderPay
 
   useEffect(() => {
-    dispatch(getOrderDetails(orderId))
-  }, [dispatch, orderId])
 
+        const addPayPalScript = async () => {
+            const { data: clientId } = await axios.get('/api/config/paypal')
+            const script = document.createElement('script')
+            script.type = 'text/javascript'
+            script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
+            script.async = true
+            script.onload = () => {
+                setSdkReady(true) 
+            }
+            document.body.appendChild(script)
+        }
+
+        if (!order || successPay) { 
+            dispatch({
+               type: ORDER_PAY_RESET
+           })
+            dispatch(getOrderDetails(orderId))
+        } else if (!order.isPaid) {
+           if (!window.paypal) {
+                addPayPalScript()
+           } else {
+           setSdkReady(true)
+        }
+       } 
+    }, [dispatch, orderId, order, successPay])
+
+  const successPaymentHandler = (paymentResult) => {
+        dispatch(payOrder(orderId, paymentResult))
+
+    }
+
+  
   return loading ? (
     <Loader />
   ) : error ? (
@@ -138,7 +168,17 @@ const OrderScreen = ({ match }) => {
                   <Col>Total</Col>
                   <Col>${order.totalPrice}</Col>
                 </Row>
-              </ListGroup.Item>
+                </ListGroup.Item>
+                {!order.ispaid && (
+                  <ListGroup.Item>
+                      {loadingPay && <Loader />}
+                      {!sdkReady ? <Loader /> : (
+                          <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler} />
+                          // <Button type='button' className='btn btn-block'>Pay</Button>
+                      )}
+                  </ListGroup.Item>
+                )}
+
             </ListGroup>
           </Card>
         </Col>
