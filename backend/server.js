fabeb5e1d242df1ce55eae66f268532b18386958
rohit gupta <rohitgupta.che18@itbhu.ac.in>
2021-04-18 23:11:34 +0530

    changed sendFile ../frontend

--- backend/server.js
+++ backend/server.js
@@ -37,14 +37,14 @@ app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => {
-    res.send(process.env.PAYPAL_CLIENT_ID);
+  res.send(process.env.PAYPAL_CLIENT_ID);
 })
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')))
-  
+
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
   })
 }
 
